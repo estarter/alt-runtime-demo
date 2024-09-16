d8ebcafaa304d7506e467c1b5ab91bfdfcae4aaf
Alexey <estarter@google.com>
2024-09-16 16:24:00 +0200

    init

---
+++ src/main/java/com/google/alt_runtime_demo/AltRuntimeDemoApplication.java
@@ -0,0 +1,13 @@
+package com.google.alt_runtime_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltRuntimeDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltRuntimeDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/google/alt_runtime_demo/AltRuntimeDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.google.alt_runtime_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AltRuntimeDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
