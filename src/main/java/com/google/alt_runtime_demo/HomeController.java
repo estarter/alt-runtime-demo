c16d24bf6227e9439934ff86699afebc6773c702
Alexey <estarter@google.com>
2024-09-16 22:48:22 +0200

    smth almost working

--- src/main/java/com/google/alt_runtime_demo/HomeController.java
+++ src/main/java/com/google/alt_runtime_demo/HomeController.java
@@ -1,6 +1,10 @@
 package com.google.alt_runtime_demo;
 
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,7 +13,48 @@ public class HomeController {
 
     @RequestMapping(value = "/test")
     @ResponseBody
-    public String home(@RequestBody(required = false) String json) {
+    public String test(@RequestBody(required = false) String json) {
         return "asdf";
     }
+
+    @PostMapping("/home")
+    @ResponseBody
+    public String onViewMessage(@RequestBody JsonNode event) {
+//        ObjectMapper response = new ObjectMapper();
+//        ObjectNode responseNode = response.createObjectNode();
+//        responseNode.put("sections", "asdf");
+//        return responseNode;
+        return """
+                {
+                    action: {
+                        navigations: [{
+                            updateCard: {
+                                  "header": {
+                                    "title": "Test card",
+                                    "imageUrl": "https://developers.google.com/chat/images/quickstart-app-avatar.png",
+                                    "imageType": "CIRCLE"
+                                  },
+                                  "sections": [
+                                    {
+                                      "header": "Section Header",
+                                      "widgets": [
+                                        {
+                                          "textParagraph": {
+                                            "text": "See <a href=https://developers.google.com/apps-script/add-ons/concepts/widgets#text_formatting>this doc</a> for rich text formatting"
+                                          }
+                                        }
+                                      ]
+                                    }
+                                  ]
+                            }
+                        }]
+                    }
+                }
+                """;
+    }
+
+    /*
+
+
+     */
 }
